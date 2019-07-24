a42554cf6828a91463335faeca290a1dfaa9307d
jcorea <jacorea410@gmail.com>
2019-07-24 13:34:36 -0700

    made list of movie list from json received from tvmaze api.

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Series from '../../Containers/Series';
 import Intro from '../Intro';
 import Time from '../Time';
 import './App.css';
@@ -6,22 +7,14 @@ import 'whatwg-fetch';
 
 
 class App extends Component {
-  state = {
-    series: [],
-  }
-
-  componentDidMount() {
-    fetch('http://api.tvmaze.com/search/shows?q=law+%26+order')
-      .then(response => response.json())
-        .then(json => this.setState({ series: json}))
-  }
+ 
 
 
   render() {
     return (
       <div className="App">
-        <Intro message="Here you can find all of your most loved series"/>
-        The length of series array - {this.state.series.length}        
+        <Intro message="Here you can find all of your most loved series"/>     
+        <Series />  
         <Time />
       </div>
     );
