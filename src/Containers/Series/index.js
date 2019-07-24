2ecd0197ed651537d3397c0f4b14bd599f1b8928
jcorea <jacorea410@gmail.com>
2019-07-24 14:11:29 -0700

    created input box as a search box to look for movie listing.

--- src/Containers/Series/index.js
+++ src/Containers/Series/index.js
@@ -6,17 +6,25 @@ export class Series extends Component {
         series: [],
         }
 
-    componentDidMount() {
-    fetch('http://api.tvmaze.com/search/shows?q=law+%26+order')
-        .then(response => response.json())
-        .then(json => this.setState({ series: json}))
+    onSeriesChange = e => {
+        fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
+            .then(response => response.json())
+            .then(json => this.setState({ series: json}))
     }
+    
+    
 
     render() {
         return (
             <div>
-                The length of series array - {this.state.series.length} 
+                <div>
+                    <input type="text" onChange={this.onSeriesChange}></input>
+                </div>
+                
                 <SeriesList list={this.state.series} />
+                <div>
+                    The length of series array - {this.state.series.length}
+                </div>
             </div>
         )
     }
