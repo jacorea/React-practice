75390a243144c18ed3358a092e8e502b50c7ee14
jcorea <jacorea410@gmail.com>
2019-07-30 11:50:10 -0700

    Add route and Link to list of series displayed.

--- src/Containers/SingleSeries/index.js
+++ src/Containers/SingleSeries/index.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
+import Loader from '../../Components/Loader'
 
 export class SingleSeries extends Component {
-  render() {
-      console.log(this.props)
+    state = {
+        show: null
+    }
+
+
+    componentDidMount() {
+        const { id } = this.props.match.params;
+        fetch(`http://api.tvmaze.com/shows/${id}?embed=episodes`)
+            .then(response => response.json())
+            .then(json => this.setState({ show: json }))
+    }
+
+
+    render() {
+      const { show } = this.state;
+      console.log(show);
     return (
       <div>
-        <p>Single Series - the show id will be {this.props.match.params.id}</p>
+        {show === null && <Loader />}
+        {
+            show !== null 
+            &&
+            <p>Show has been loaded.</p>
+        }
       </div>
     )
   }
